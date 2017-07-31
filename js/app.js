98080be43ba99c4315f844b71db2e7e475a98849
nancyzj <ancizj@hotmail.com>
2017-07-31 08:46:22 -0500

    parte principal con mejoras

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-(function(argument) {
+(function() {
 	
+	var splash = function () {
+		var splashScreen = document.querySelector('.splash');
+		var home = document.querySelector('.container');
+		splashScreen.style.display = 'none';
+		home.classList.remove('hidden');
+	}
 
+	setTimeout(splash, 3000);
 
-	$(document).ready(CargarPagina);
 })();
