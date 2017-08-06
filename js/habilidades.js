e9e61fd2a9f8bff1990fda98b6f321c993f0b043
nancyzj <ancizj@hotmail.com>
2017-08-06 17:43:47 -0500

    avances en skill

--- js/habilidades.js
+++ js/habilidades.js
@@ -1,16 +1,24 @@
 var leerPagina = function() {
 	var rating = [30, 60, 95,80];
 	var ratingBar = document.querySelectorAll(".js-rating-progress");
-	document.querySelector('#walk-score').innerHTML = rating[0];
-	document.querySelector('#transit-score').innerHTML = rating[1];
-	document.querySelector('#bike-score').innerHTML = rating[2];
-	document.querySelector('#javascript').innerHTML = rating[3];
+	document.querySelector('#walk-score').innerHTML = "Intermedio";
+	document.querySelector('#transit-score').innerHTML = "Intermedio";
+	document.querySelector('#bike-score').innerHTML = "Intermedio";
+	document.querySelector('#javascript').innerHTML = "Básico";
+	document.querySelector('#bootstrap').innerHTML = "Intermedio";
+	document.querySelector('#materializecss').innerHTML = "Intermedio";
+	document.querySelector('#react').innerHTML = "Básico";
+	document.querySelector('#jQuery').innerHTML = "Intermedio";
+	
 
-	document.querySelector('.walk-container .js-rating-progress').style.strokeDashoffset = 100 - rating[0];
+	document.querySelector('.walk-container .js-rating-progress').style.strokeDashoffset = 100 - rating[1];
 	document.querySelector('.transit-container .js-rating-progress').style.strokeDashoffset = 100 - rating[1];
-	document.querySelector('.bike-container .js-rating-progress').style.strokeDashoffset = 100 - rating[2];
-	document.querySelector('.javascript .js-rating-progress').style.strokeDashoffset = 100 - rating[3];
-
+	document.querySelector('.bike-container .js-rating-progress').style.strokeDashoffset = 100 - rating[1];
+	document.querySelector('.javascript .js-rating-progress').style.strokeDashoffset = 100 - rating[0];
+	document.querySelector('.bootstrap .js-rating-progress').style.strokeDashoffset = 100 - rating[1];
+	document.querySelector('.materializecss .js-rating-progress').style.strokeDashoffset = 100 - rating[1];
+	document.querySelector('.react .js-rating-progress').style.strokeDashoffset = 100 - rating[0];
+	document.querySelector('.jQuery .js-rating-progress').style.strokeDashoffset = 100 - rating[0];
 	for (i=0; i<ratingBar.length; i++) {
 	  if (rating[i] <= 50) {
 	    ratingBar[i].classList.add('under-50');
